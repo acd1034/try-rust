55fefced85451e40bfd441a17b77c9aee93bf917
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-11-01 21:52:41 +0900

    Complete itp1_4c

--- draft/itp1_4c.rs
+++ draft/itp1_4c.rs
@@ -1,27 +1,24 @@
-use std::io;
+use std::io::stdin;
 
 fn read_expr() -> Option<i32> {
   let mut input = String::new();
-  match io::stdin().read_line(&mut input) {
-    Ok(_) => {
-      let toks: Vec<_> = input.split(' ').collect();
-      // println!("{:?}", toks);
-      toks[0].parse::<i32>().ok().and_then(|x| {
-        toks[2]
-          .trim()
-          .parse::<i32>()
-          .ok()
-          .and_then(|y| match toks[1] {
-            "+" => Some(x + y),
-            "-" => Some(x - y),
-            "*" => Some(x * y),
-            "/" => Some(x / y),
-            _ => None,
-          })
-      })
-    }
-    Err(_) => None,
-  }
+  stdin().read_line(&mut input).ok().and_then(|_| {
+    let toks: Vec<_> = input.split_whitespace().collect();
+    // println!("{:?}", toks);
+    toks[0].parse::<i32>().ok().and_then(|x| {
+      toks[2]
+        .trim()
+        .parse::<i32>()
+        .ok()
+        .and_then(|y| match toks[1] {
+          "+" => Some(x + y),
+          "-" => Some(x - y),
+          "*" => Some(x * y),
+          "/" => Some(x / y),
+          _ => None,
+        })
+    })
+  })
 }
 
 fn main() {
