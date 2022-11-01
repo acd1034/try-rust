b139ac3751c841d2b553a3828498f496da7812a0
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-11-01 22:07:29 +0900

    itp1_4c: もっと簡潔に

--- draft/itp1_4c.rs
+++ draft/itp1_4c.rs
@@ -2,23 +2,17 @@ use std::io::stdin;
 
 fn read_expr() -> Option<i32> {
   let mut input = String::new();
-  stdin().read_line(&mut input).ok().and_then(|_| {
-    let toks: Vec<_> = input.split_whitespace().collect();
-    // println!("{:?}", toks);
-    toks[0].parse::<i32>().ok().and_then(|x| {
-      toks[2]
-        .trim()
-        .parse::<i32>()
-        .ok()
-        .and_then(|y| match toks[1] {
-          "+" => Some(x + y),
-          "-" => Some(x - y),
-          "*" => Some(x * y),
-          "/" => Some(x / y),
-          _ => None,
-        })
-    })
-  })
+  stdin().read_line(&mut input).ok()?;
+  let toks: Vec<_> = input.split_whitespace().collect();
+  let x = toks[0].parse::<i32>().ok()?;
+  let y = toks[2].trim().parse::<i32>().ok()?;
+  match toks[1] {
+    "+" => Some(x + y),
+    "-" => Some(x - y),
+    "*" => Some(x * y),
+    "/" => Some(x / y),
+    _ => None,
+  }
 }
 
 fn main() {
