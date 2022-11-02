2f294dcbb99904c3f1d6d0cd7d461399daef4261
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-11-02 16:27:42 +0900

    itp1_4d: Add error massage

--- draft/itp1_4d.rs
+++ draft/itp1_4d.rs
@@ -1,24 +1,30 @@
-use std::io::stdin;
+type Expected<T> = Result<T, &'static str>;
 
-fn read_expr() -> Option<(i64, i64, i64)> {
+fn read_line() -> Expected<String> {
   let mut input = String::new();
-  stdin().read_line(&mut input).ok()?;
-  input.trim().parse::<i64>().ok()?;
-  let mut input = String::new();
-  stdin().read_line(&mut input).ok()?;
+  std::io::stdin()
+    .read_line(&mut input)
+    .map_err(|_| "failed to read line")?;
+  Ok(input.trim_end().to_string())
+}
+
+fn read_expr() -> Expected<(i64, i64, i64)> {
+  read_line()?;
+  let input = read_line()?;
   let nums: Vec<_> = input
     .split_whitespace()
-    .map(|s| s.trim().parse::<i64>().unwrap())
+    .map(|s| s.parse::<i64>().unwrap())
     .collect();
   // println!("{:?}", nums);
-  let min = nums.iter().min()?;
-  let max = nums.iter().max()?;
+  let min = nums.iter().min().ok_or("faild to calculate min")?;
+  let max = nums.iter().max().ok_or("faild to calculate max")?;
   let sum = nums.iter().fold(0, |x, y| x + y);
-  Some((min.clone(), max.clone(), sum))
+  Ok((min.clone(), max.clone(), sum))
 }
 
 fn main() {
-  if let Some((min, max, sum)) = read_expr() {
-    println!("{} {} {}", min, max, sum)
+  match read_expr() {
+    Ok((min, max, sum)) => println!("{} {} {}", min, max, sum),
+    Err(msg) => println!("{}", msg),
   }
 }
