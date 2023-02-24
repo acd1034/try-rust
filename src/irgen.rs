b31f7889e853aad3e441901c96fb35743acc342c
acd1034 <48613285+acd1034@users.noreply.github.com>
2023-02-24 18:15:25 +0900

    Add Sub

--- src/irgen.rs
+++ src/irgen.rs
@@ -58,6 +58,7 @@ impl BB {
 
 pub enum Inst {
   Add(Val, Val, Val),
+  Sub(Val, Val, Val),
   Ret(Val),
 }
 
@@ -67,6 +68,7 @@ impl fmt::Display for Inst {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     match self {
       Inst::Add(v0, v1, v2) => write!(f, "{} = add {}, {}", v0, v1, v2),
+      Inst::Sub(v0, v1, v2) => write!(f, "{} = sub {}, {}", v0, v1, v2),
       Inst::Ret(v1) => write!(f, "ret {}", v1),
     }
   }
@@ -184,6 +186,13 @@ impl GenFun {
         let v2 = self.gen_expr(*m, bb)?;
         let v0 = self.new_reg();
         self.push_inst(Inst::Add(v0.clone(), v1, v2), bb);
+        Ok(v0)
+      }
+      AST::Sub(n, m) => {
+        let v1 = self.gen_expr(*n, bb)?;
+        let v2 = self.gen_expr(*m, bb)?;
+        let v0 = self.new_reg();
+        self.push_inst(Inst::Sub(v0.clone(), v1, v2), bb);
         Ok(v0)
       }
       AST::Num(n) => Ok(Val::Imm(n)),
--- test2.sh
+++ test2.sh
@@ -40,15 +40,16 @@ assert_fail() {
 # num
 assert 0 'int main() { return 0; }'
 assert 42 'int main() { return 42; }'
-assert 42 'int main() { return 1+2; }'
-assert 42 'int main() { return 1+2+3; }'
+# add
+assert 3 'int main() { return 1+2; }'
+assert 1 'int main() { return 2-1; }'
+assert 10 'int main() { return 1+2+3+4; }'
+assert 4 'int main() { return 1+2-3+4; }'
 exit
 # term
 assert 24 'int main() { return 1*2*3*4; }'
 assert 4 'int main() { return 3*4/6*2; }'
 # expr
-assert 10 'int main() { return 1+2+3+4; }'
-assert 4 'int main() { return 1+2-3+4; }'
 assert 44 'int main() { return 1*2+3*4+5*6; }'
 assert 20 'int main() { return 1*2-6/3+4*5; }'
 # primary
