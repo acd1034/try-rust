aaf45c0581d968ef783350a6b78ffb40a03ee75b
acd1034 <48613285+acd1034@users.noreply.github.com>
2023-05-31 20:16:06 +0900

    Fix irdump

--- src/ir/irdump.rs
+++ src/ir/irdump.rs
@@ -2,130 +2,144 @@ use crate::common::JoinView;
 use crate::ir::function::*;
 use crate::ir::inst::*;
 use crate::ir::module::*;
-use id_arena::Arena;
 use std::fmt;
 
 impl fmt::Display for Module {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    irdump(f, self)
+    DumpIr::new(self, f).run_on_module()
   }
 }
 
-fn irdump(f: &mut fmt::Formatter, module: &Module) -> fmt::Result {
-  write!(f, "; ModuleName = '{}'", module.name())?;
-  for (_id, fun) in module.functions().iter() {
-    dump_fun(f, fun, module.functions())?;
-  }
-  Ok(())
+pub struct DumpIr<'a, 'b> {
+  module: &'a Module,
+  f: &'a mut fmt::Formatter<'b>,
 }
 
-fn dump_fun(f: &mut fmt::Formatter, fun: &Function, functions: &Arena<Function>) -> fmt::Result {
-  let iter = fun.param_tys().iter();
-  let param_tys = JoinView::new(iter, ", ");
-
-  if fun.is_declaration() {
-    return write!(f, "\n\ndeclare {}({})", fun.name(), param_tys);
+impl<'a, 'b> DumpIr<'a, 'b> {
+  pub fn new(module: &'a Module, f: &'a mut fmt::Formatter<'b>) -> DumpIr<'a, 'b> {
+    DumpIr { module, f }
   }
 
-  // Emit function return type, name and parameters
-  write!(f, "\n\n{}({}):", fun.name(), param_tys)?;
-
-  // Allocate memory
-  if fun.memory_arena().len() != 0 {
-    write!(f, "\n  m = alloca {}", fun.memory_arena().len())?;
-  }
-
-  // Emit function body
-  for &block_id in fun.blocks() {
-    let block_label = format!(".block{}:", block_id.index());
-    // let pred = JoinView::new(fun.get(block_id).pred().iter(), ",");
-    // let succ = JoinView::new(fun.get(block_id).succ().iter(), ",");
-    write!(f, "\n{}", block_label)?;
-    // write!(f, "\n{:<40}; pred={} succ={}", block_label, pred, succ)?;
-    for &inst_id in fun.get(block_id).insts() {
-      dump_inst(f, &fun.get(inst_id), functions)?;
+  pub fn run_on_module(&mut self) -> fmt::Result {
+    write!(self.f, "; ModuleName = '{}'", self.module.name())?;
+    for (_id, fun) in self.module.functions().iter() {
+      self.run_on_function(fun)?;
     }
+    Ok(())
   }
 
-  Ok(())
-}
+  fn run_on_function(&mut self, fun: &Function) -> fmt::Result {
+    let iter = fun.param_tys().iter();
+    let param_tys = JoinView::new(iter, ", ");
 
-fn dump_inst(f: &mut fmt::Formatter, inst: &Inst, functions: &Arena<Function>) -> fmt::Result {
-  match inst.kind() {
-    InstKind::Eq(v0, v1, v2) => write!(
-      f,
-      "\n  r{} = eq r{}, r{}",
-      v0.index(),
-      v1.index(),
-      v2.index()
-    ),
-    InstKind::Ne(v0, v1, v2) => write!(
-      f,
-      "\n  r{} = ne r{}, r{}",
-      v0.index(),
-      v1.index(),
-      v2.index()
-    ),
-    InstKind::Lt(v0, v1, v2) => write!(
-      f,
-      "\n  r{} = lt r{}, r{}",
-      v0.index(),
-      v1.index(),
-      v2.index()
-    ),
-    InstKind::Le(v0, v1, v2) => write!(
-      f,
-      "\n  r{} = le r{}, r{}",
-      v0.index(),
-      v1.index(),
-      v2.index()
-    ),
-    InstKind::Add(v0, v1, v2) => write!(
-      f,
-      "\n  r{} = add r{}, r{}",
-      v0.index(),
-      v1.index(),
-      v2.index()
-    ),
-    InstKind::Sub(v0, v1, v2) => write!(
-      f,
-      "\n  r{} = sub r{}, r{}",
-      v0.index(),
-      v1.index(),
-      v2.index()
-    ),
-    InstKind::Mul(v0, v1, v2) => write!(
-      f,
-      "\n  r{} = mul r{}, r{}",
-      v0.index(),
-      v1.index(),
-      v2.index()
-    ),
-    InstKind::Div(v0, v1, v2) => write!(
-      f,
-      "\n  r{} = div r{}, r{}",
-      v0.index(),
-      v1.index(),
-      v2.index()
-    ),
-    InstKind::Call(v0, fun, args) => {
-      let args = JoinView::new(args.iter().map(|id| format!("r{}", id.index())), ", ");
-      write!(
-        f,
-        "\n  r{} = call {}({});",
+    if fun.is_declaration() {
+      return write!(self.f, "\n\ndeclare {}({})", fun.name(), param_tys);
+    }
+
+    // Emit function return type, name and parameters
+    write!(self.f, "\n\n{}({}):", fun.name(), param_tys)?;
+
+    // Allocate memory
+    if fun.memory_arena().len() != 0 {
+      write!(self.f, "\n  m = alloca {}", fun.memory_arena().len())?;
+    }
+
+    // Emit function body
+    for &block_id in fun.blocks() {
+      let block_label = format!(".block{}:", block_id.index());
+      let pred = JoinView::new(fun.get(block_id).pred().iter().map(|id| id.index()), ",");
+      let succ = JoinView::new(fun.get(block_id).succ().iter().map(|id| id.index()), ",");
+      write!(self.f, "\n{:<40}; pred={} succ={}", block_label, pred, succ)?;
+      for &inst_id in fun.get(block_id).insts() {
+        self.run_on_inst(fun.get(inst_id))?;
+      }
+    }
+
+    Ok(())
+  }
+
+  fn run_on_inst(&mut self, inst: &Inst) -> fmt::Result {
+    match inst.kind() {
+      InstKind::Eq(v0, v1, v2) => write!(
+        self.f,
+        "\n  r{} = eq r{}, r{}",
         v0.index(),
-        functions.get(*fun).unwrap().name(),
-        args
-      )
+        v1.index(),
+        v2.index()
+      ),
+      InstKind::Ne(v0, v1, v2) => write!(
+        self.f,
+        "\n  r{} = ne r{}, r{}",
+        v0.index(),
+        v1.index(),
+        v2.index()
+      ),
+      InstKind::Lt(v0, v1, v2) => write!(
+        self.f,
+        "\n  r{} = lt r{}, r{}",
+        v0.index(),
+        v1.index(),
+        v2.index()
+      ),
+      InstKind::Le(v0, v1, v2) => write!(
+        self.f,
+        "\n  r{} = le r{}, r{}",
+        v0.index(),
+        v1.index(),
+        v2.index()
+      ),
+      InstKind::Add(v0, v1, v2) => write!(
+        self.f,
+        "\n  r{} = add r{}, r{}",
+        v0.index(),
+        v1.index(),
+        v2.index()
+      ),
+      InstKind::Sub(v0, v1, v2) => write!(
+        self.f,
+        "\n  r{} = sub r{}, r{}",
+        v0.index(),
+        v1.index(),
+        v2.index()
+      ),
+      InstKind::Mul(v0, v1, v2) => write!(
+        self.f,
+        "\n  r{} = mul r{}, r{}",
+        v0.index(),
+        v1.index(),
+        v2.index()
+      ),
+      InstKind::Div(v0, v1, v2) => write!(
+        self.f,
+        "\n  r{} = div r{}, r{}",
+        v0.index(),
+        v1.index(),
+        v2.index()
+      ),
+      InstKind::Load(v0, m1) => write!(self.f, "\n  r{} = load m{}", v0.index(), m1.index()),
+      InstKind::Call(v0, fun_id, args) => {
+        let args = JoinView::new(args.iter().map(|id| format!("r{}", id.index())), ", ");
+        write!(
+          self.f,
+          "\n  r{} = call {}({});",
+          v0.index(),
+          self.module.functions_get(*fun_id).name(),
+          args
+        )
+      }
+      InstKind::Const(v0, n) => write!(self.f, "\n  r{} = const {}", v0.index(), n),
+      InstKind::Br(v1, block1, block2) => {
+        write!(
+          self.f,
+          "\n  br r{}, block{}, block{}",
+          v1.index(),
+          block1.index(),
+          block2.index()
+        )
+      }
+      InstKind::Jmp(block1) => write!(self.f, "\n  jmp block{}", block1.index()),
+      InstKind::Store(m1, v2) => write!(self.f, "\n  store m{}, r{}", m1.index(), v2.index()),
+      InstKind::Ret(v1) => write!(self.f, "\n  ret r{}", v1.index()),
     }
-    InstKind::Const(v0, n) => write!(f, "\n  r{} = const {}", v0.index(), n),
-    // InstKind::Br(v1, block1, block2) => {
-    //   write!(f, "\n  br {}, block{}, block{}", v1, block1, block2)
-    // }
-    // InstKind::Jmp(block1) => write!(f, "\n  jmp block{}", block1),
-    // InstKind::Store(m1, v2) => write!(f, "\n  store m{}, {}", m1, v2),
-    // InstKind::Load(m1) => write!(f, "\n  r{} = load m{}", v0.index(), m1),
-    // InstKind::Ret(v1) => write!(f, "\n  ret {}", v1),
-    _ => todo!(),
   }
 }
